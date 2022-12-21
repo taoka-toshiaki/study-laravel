a35d0b285701da21439badc74f43b82b07a3f4dc
taoka-toshiaki <mail@taoka-toshiaki.com>
2022-12-21 10:53:11 +0900

    リクエスト1

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Http\Requests\HelloRequest;
 
 class SubmitController extends Controller
 {
     //
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'txt'=>'required',
-            'mail'=>'email',
-            'age'=>'numeric|between:0,99'
-        ];
-        $this->validate($request,$validate_rule);
-        return view("welcome",['msg'=>'OK']);
+        // $validate_rule = [
+        //     'txt'=>'required',
+        //     'mail'=>'email',
+        //     'age'=>'numeric|between:0,99'
+        // ];
+        // $this->validate($request,$validate_rule);
+        return view("welcome",['msg'=>'ok']);
     }
 }
