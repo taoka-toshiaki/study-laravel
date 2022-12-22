8a2048a9870a505dfc302b2710633f3af8019379
taoka-toshiaki <mail@taoka-toshiaki.com>
2022-12-22 09:25:56 +0900

    バリデータ1

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator;
 
 class SubmitController extends Controller
 {
     //
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
         // $validate_rule = [
         //     'txt'=>'required',
@@ -16,6 +17,14 @@ class SubmitController extends Controller
         //     'age'=>'numeric|between:0,99'
         // ];
         // $this->validate($request,$validate_rule);
+        $validator = Validator::make($request->all(),[
+            'txt'=>'required',
+            'mail'=>'email',
+            'age'=>'numeric|between:0,99'
+        ]);
+        if($validator->fails()){
+            return redirect('/')->withErrors($validator)->withInput();
+        }
         return view("welcome",['msg'=>'ok']);
     }
 }
