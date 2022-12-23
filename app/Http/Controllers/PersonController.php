befdaae3b6d59b96f645a7651e49d40f927fef34
taoka-toshiaki <mail@taoka-toshiaki.com>
2022-12-23 15:10:45 +0900

    モデル3

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -14,7 +14,8 @@ class PersonController extends Controller
         $item = null;
         
         if(isset($request->id)){
-            $item = Person::find(((int)$request->id));
+            // $item = Person::find(((int)$request->id));
+            $items = Person::orwhere('id',"=",(int)$request->id)->orwhere('mail','like','%gmail%')->orderBy('id','desc')->get();
         }else{
             $items = Person::all();
         }
